62cc9260b8ef58189dbf360bbcb238d87a2c58c9
あずき⪥™ <44765629+melt-adzuki@users.noreply.github.com>
2023-03-09 21:28:12 +0900

    feat: filter a functional reaction

--- src/config.ts
+++ src/config.ts
@@ -8,17 +8,20 @@ export interface Env {
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	// MY_BUCKET: R2Bucket;
 
+	SLACK_REACTION: string
 	SLACK_BOT_TOKEN: string
 	NOTION_TOKEN: string
 	NOTION_DATABASE_ID: string
 }
 
 class Config implements Env {
+	public SLACK_REACTION!: string
     public SLACK_BOT_TOKEN!: string
     public NOTION_TOKEN!: string
     public NOTION_DATABASE_ID!: string
 
-    public init(env: Env) {
+	public init(env: Env) {
+		this.SLACK_REACTION = env.SLACK_REACTION
         this.SLACK_BOT_TOKEN = env.SLACK_BOT_TOKEN
         this.NOTION_TOKEN = env.NOTION_TOKEN
         this.NOTION_DATABASE_ID = env.NOTION_DATABASE_ID
--- src/presentation/router/ReactionAdded.ts
+++ src/presentation/router/ReactionAdded.ts
@@ -4,9 +4,14 @@ import { container } from "tsyringe"
 import AddTaskService from "../../application/services/AddTaskService"
 import Message from "../models/Message"
 import WorkspaceUrl from "../models/WorkspaceUrl"
+import config from "../../config"
 
 export default class ReactionAdded extends EventCallback<"reaction_added"> {
     async handle(): Promise<ResponseProvider> {
+        if (this.event.reaction !== config.SLACK_REACTION) {
+            return ResponseProvider.succeed(`The reaction :${this.event.reaction}: is not the target.`)
+        }
+
         const message = await Message.fromTimestamp(this.event.item.ts, this.event.item.channel)
         const workspaceUrl = await WorkspaceUrl.fetch()
 
